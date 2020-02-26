444cd970ba84c840aad4a8a181c08fb025fea816
yapdianhao1011 <yapdhao@gmail.com>
2020-02-26 14:23:13 +0800

    added RemarkCommand.java

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,6 +1,9 @@
 package seedu.address.logic.commands;
 
 import seedu.address.model.Model;
+import seedu.address.logic.commands.exceptions.CommandException;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import seedu.address.commons.core.index.Index;
 
 /**
  * Changes the remark of an existing person in the address book.
@@ -9,8 +12,52 @@ public class RemarkCommand extends Command {
 
     public static final String COMMAND_WORD = "remark";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified " +
+                     "by the index number used in the last person listing. " +
+                    "Existing remark will be overwritten by the input.\n" +
+                    "Parameters: INDEX (must be a positive integer) " +
+                    "r/ [REMARK]\n" +
+                    "Example: " + COMMAND_WORD + " 1 " +
+                    "r/ Likes to swim.";
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
+
+    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
+
+    private final Index index;
+
+    private final String remark;
+
+    public static final String MESSAGE_ADD_REMARK_SUCCESS = "Added remark to Person: %1$s";
+
+    public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Removed remark from Person: %1$s";
+
+    public RemarkCommand(Index index, String remark) {
+        requireAllNonNull(index, remark);
+
+        this.index = index;
+        this.remark = remark;
+    }
+
     @Override
-    public CommandResult execute(Model model) {
-        return new CommandResult("Hello from remark");
+    public CommandResult execute(Model model) throws CommandException {
+        throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), remark));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof RemarkCommand)) {
+            return false;
+        }
+
+        // state check
+        RemarkCommand e = (RemarkCommand) other;
+        return index.equals(e.index)
+                && remark.equals(e.remark);
     }
 }
